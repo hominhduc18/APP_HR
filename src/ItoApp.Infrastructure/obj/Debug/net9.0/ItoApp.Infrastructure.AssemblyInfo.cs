26c63bb67f9fffe47e2849ef1d231ad1661b84e7
hominhduc18 <hoduc092@gmail.com>
2026-01-18 09:06:35 +0700

    create project

--- src/ItoApp.Infrastructure/obj/Debug/net9.0/ItoApp.Infrastructure.AssemblyInfo.cs
+++ src/ItoApp.Infrastructure/obj/Debug/net9.0/ItoApp.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItoApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbcbd5adcc9ff0cc4430ccd58963b39cafece18f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ef6ce19e63847027060beb86ddee9697362d3b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItoApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItoApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
